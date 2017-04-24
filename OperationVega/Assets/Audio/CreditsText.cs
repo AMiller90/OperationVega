52ec5b70dde972269bb2d69fe287fb13587eb043
Andrew Miller <A_Miller729@yahoo.com>
2017-04-24 17:45:44 -0500

    Update credits menu
    Started working on the credits menu. Updated the credits panel and set
    the text to scale in accordance to the screen.

---
+++ OperationVega/Assets/Audio/CreditsText.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CreditsText : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        this.canrotate = true;
+    }
+
+    private bool canrotate;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if(this.canrotate)
+            this.transform.Rotate(Vector3.down, Time.deltaTime * 50);
+    }
+}
